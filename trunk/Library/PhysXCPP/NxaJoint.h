1366f7c7c9c8692be8bea7b9f10dedbc340face7
michael.brooks11 <michael.brooks11@e8b6d1ee-b643-0410-9178-bfabf5f736f5>
2008-02-01 21:04:11 +0000

    Wrapped getGlobalAnchor and Axis methods and setBreakable in NxaJoint. If it doesn't build, it's probably because I had to redefine the extra #include directories to point to C:\ instead of S:\.

--- trunk/Library/PhysXCPP/NxaJoint.h
+++ trunk/Library/PhysXCPP/NxaJoint.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "NxaMath.h"
+
 #include "NxJoint.h"
 
 public ref class NxaJoint abstract
@@ -9,4 +11,12 @@ internal:
 		
 public:
 	NxaJoint(void);
+
+	void setGlobalAnchor(Vector3 vec);
+	void setGlobalAxis(Vector3 vec);
+	Vector3 getGlobalAnchor();
+	Vector3 getGlobalAxis();
+
+	//NxJointState getState();
+	void setBreakable(float maxForce, float maxTorque);
 };
