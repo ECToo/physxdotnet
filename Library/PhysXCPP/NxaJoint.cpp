7bea2dc18fe1dea64e3927a8d9ae7720ce4dc6f5
michael.brooks11 <michael.brooks11@e8b6d1ee-b643-0410-9178-bfabf5f736f5>
2008-02-02 08:10:33 +0000

    Filled in NxaJoint substantially. Project does not build because of constructors in NxaSphericalJoint and NxaFixedJoint.

--- Library/PhysXCPP/NxaJoint.cpp
+++ Library/PhysXCPP/NxaJoint.cpp
@@ -2,34 +2,155 @@
 #include "NxaJoint.h"
 
 
-NxaJoint::NxaJoint(void)
+NxaJoint::NxaJoint(NxJoint* ptr)
 {
-
+	nxJoint = ptr;
 }
 
-void NxaJoint::setGlobalAnchor(Microsoft::Xna::Framework::Vector3 vec)
+NxaJoint^ NxaJoint::CreateFromPointer(NxJoint* ptr)
+{
+	if(ptr != NULL)
+	{
+		switch(ptr->getType())
+		{
+		case NX_JOINT_PRISMATIC :
+			throw gcnew Exception("NOT IMPLEMENTED PROPERLY!");
+			return nullptr;
+		case NX_JOINT_REVOLUTE :
+			throw gcnew Exception("NOT IMPLEMENTED PROPERLY!");
+			return nullptr;
+		case NX_JOINT_SPHERICAL :
+			//return gcnew NxaSphericalJoint(ptr); <-- Temporarily out of service
+		case NX_JOINT_POINT_ON_LINE :
+			throw gcnew Exception("NOT IMPLEMENTED PROPERLY!");
+			return nullptr;
+		case NX_JOINT_POINT_IN_PLANE :
+			throw gcnew Exception("NOT IMPLEMENTED PROPERLY!");
+			return nullptr;
+		case NX_JOINT_DISTANCE :
+			throw gcnew Exception("NOT IMPLEMENTED PROPERLY!");
+			return nullptr;
+		case NX_JOINT_PULLEY :
+			throw gcnew Exception("NOT IMPLEMENTED PROPERLY!");
+			return nullptr;
+		case NX_JOINT_FIXED :
+			//return gcnew NxaFixedJoint(ptr); <-- Temporarily out of service
+		case NX_JOINT_D6 :
+			throw gcnew Exception("NOT IMPLEMENTED PROPERLY!");
+			return nullptr;
+		}
+	}
+
+	return nullptr;
+}
+
+void NxaJoint::SetGlobalAnchor(Vector3 vec)
 {
 	nxJoint->setGlobalAnchor(NxaMath::Vector3XNAToPhysX(vec));
 }
 
-void NxaJoint::setGlobalAxis(Microsoft::Xna::Framework::Vector3 vec)
+void NxaJoint::SetGlobalAxis(Vector3 vec)
 {
 	nxJoint->setGlobalAxis(NxaMath::Vector3XNAToPhysX(vec));
 }
 
-Vector3 NxaJoint::getGlobalAnchor()
+Vector3 NxaJoint::GetGlobalAnchor()
 {
 	return NxaMath::Vector3PhysXToXNA(nxJoint->getGlobalAnchor());
 }
 
-Vector3 NxaJoint::getGlobalAxis()
+Vector3 NxaJoint::GetGlobalAxis()
 {
 	return NxaMath::Vector3PhysXToXNA(nxJoint->getGlobalAxis());
 }
 
-//NxJointState getState(); <-- recreate this enum?
+NxaJointState NxaJoint::GetState()
+{
+	return (NxaJointState)nxJoint->getState();
+}
 
-void NxaJoint::setBreakable(float maxForce, float maxTorque)
+void NxaJoint::SetBreakable(float maxForce, float maxTorque)
 {
 	nxJoint->setBreakable(maxForce, maxTorque);
+}
+
+void NxaJoint::GetBreakable([Out] float maxForce, [Out] float maxTorque)
+{
+	nxJoint->getBreakable(maxForce, maxTorque);
+}
+
+/********Limits**********/
+
+void NxaJoint::SetLimitPoint(Vector3 point, bool pointIsOnActor2)
+{
+	nxJoint->setLimitPoint(NxaMath::Vector3XNAToPhysX(point), pointIsOnActor2);
+}
+
+void NxaJoint::SetLimitPoint(Vector3 point)
+{
+	nxJoint->setLimitPoint(NxaMath::Vector3XNAToPhysX(point));
+}
+
+bool NxaJoint::GetLimitPoint([Out] Vector3% worldLimitPoint)
+{
+	NxVec3 nxV;
+	bool r = nxJoint->getLimitPoint(nxV);
+	NxaMath::Vector3PhysXToXNA(nxV, worldLimitPoint);
+	return r;
+}
+
+bool NxaJoint::AddLimitPlane(Vector3 normal, Vector3 pointInPlane, float restitution)
+{
+	return nxJoint->addLimitPlane(NxaMath::Vector3XNAToPhysX(normal), NxaMath::Vector3XNAToPhysX(pointInPlane), restitution);
+}
+
+bool NxaJoint::AddLimitPlane(Vector3 normal, Vector3 pointInPlane)
+{
+	return nxJoint->addLimitPlane(NxaMath::Vector3XNAToPhysX(normal), NxaMath::Vector3XNAToPhysX(pointInPlane));
+}
+
+bool NxaJoint::GetNextLimitPlane([Out] Vector3% planeNormal, [Out] float planeD, [Out] float restitution)
+{
+	NxVec3 nxV;
+	bool r = nxJoint->getNextLimitPlane(nxV, planeD, &restitution);
+	NxaMath::Vector3PhysXToXNA(nxV, planeNormal);
+	return r;
+}
+
+/************************/
+
+bool NxaJoint::Is(NxaJointType type)
+{
+	switch(type)
+	{
+	case NxaJointType::Prismatic : return (this->IsPrismaticJoint());
+	case NxaJointType::Revolute : return (this->IsRevoluteJoint());
+	case NxaJointType::Cylindrical : return (this->IsCylindricalJoint());
+	case NxaJointType::Spherical : return (this->IsSphericalJoint());
+	case NxaJointType::PointOnLine : return (this->IsPointOnLineJoint());
+	case NxaJointType::PointInPlane : return (this->IsPointInPlaneJoint());
+	case NxaJointType::Distance : return (this->IsDistanceJoint());
+	case NxaJointType::Pulley : return (this->IsPulleyJoint());
+	case NxaJointType::Fixed : return (this->IsFixedJoint());
+	case NxaJointType::D6 : return (this->IsD6Joint());
+	}
+
+	return false;
+}
+
+/************************/
+
+void NxaJoint::SetName(String^ name)
+{
+	jointName = name;
+}
+
+String^ NxaJoint::GetName()
+{
+	return jointName;
+}
+
+NxaScene^ NxaJoint::GetScene()
+{
+	return %NxaScene(&(nxJoint->getScene()));
 }
--- Library/PhysXCPP/NxaJoint.h
+++ Library/PhysXCPP/NxaJoint.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "NxaMath.h"
+#include "NxaScene.h"
+#include "Nxap.h"
 
 #include "NxJoint.h"
 
@@ -8,15 +10,53 @@ public ref class NxaJoint abstract
 {
 internal:
 	NxJoint* nxJoint;
+	String^ jointName;
+
+	NxaJoint(NxJoint* ptr);
+	
+	static NxaJoint^ CreateFromPointer(NxJoint* ptr);
 		
 public:
-	NxaJoint(void);
 
-	void setGlobalAnchor(Vector3 vec);
-	void setGlobalAxis(Vector3 vec);
-	Vector3 getGlobalAnchor();
-	Vector3 getGlobalAxis();
+	void SetGlobalAnchor(Vector3 vec);
+	void SetGlobalAxis(Vector3 vec);
+	Vector3 GetGlobalAnchor();
+	Vector3 GetGlobalAxis();
 
-	//NxJointState getState();
-	void setBreakable(float maxForce, float maxTorque);
+	NxaJointState GetState();
+	void SetBreakable(float maxForce, float maxTorque);
+	void GetBreakable([Out] float maxForce,[Out] float maxTorque); //<-- doesn't work yet for floats
+
+	/********Limits**********/
+
+	void SetLimitPoint(Vector3 point, bool pointIsOnActor2);
+	void SetLimitPoint(Vector3 point); //overload with default boolean = true
+	bool GetLimitPoint([Out] Vector3% worldLimitPoint);
+	bool AddLimitPlane(Vector3 normal, Vector3 pointInPlane, float restitution);
+	bool AddLimitPlane(Vector3 normal, Vector3 pointInPlane);
+	void PurgeLimitPlanes() { nxJoint->purgeLimitPlanes(); }
+	void ResetLimitPlaneIterator() { nxJoint->resetLimitPlaneIterator(); }
+	bool HasMoreLimitPlanes() { return nxJoint->hasMoreLimitPlanes(); }
+	bool GetNextLimitPlane([Out] Vector3% planeNormal, [Out] float planeD, [Out] float restitution); //<--doesn't work yet for floats
+
+	/********Types***********/
+
+	NxJointType GetType() { return nxJoint->getType(); }
+	bool Is(NxaJointType type);
+	bool IsPrismaticJoint() { return (nxJoint->isPrismaticJoint() != NULL); }
+	bool IsRevoluteJoint() { return (nxJoint->isRevoluteJoint() != NULL); }
+	bool IsCylindricalJoint() { return (nxJoint->isCylindricalJoint() != NULL); }
+	bool IsSphericalJoint() { return (nxJoint->isSphericalJoint() != NULL); }
+	bool IsPointOnLineJoint() { return (nxJoint->isPointOnLineJoint() != NULL); }
+	bool IsPointInPlaneJoint() { return (nxJoint->isPointInPlaneJoint() != NULL); }
+	bool IsDistanceJoint() { return (nxJoint->isDistanceJoint() != NULL); }
+	bool IsPulleyJoint() { return (nxJoint->isPulleyJoint() != NULL); }
+	bool IsFixedJoint() { return (nxJoint->isFixedJoint() != NULL); }
+	bool IsD6Joint() { return (nxJoint->isD6Joint() != NULL); }
+
+	/********Name/Info*******/
+	void SetName(String^ name);
+	String^ GetName();
+	NxaScene^ GetScene();
+	Object^ userData;
 };
