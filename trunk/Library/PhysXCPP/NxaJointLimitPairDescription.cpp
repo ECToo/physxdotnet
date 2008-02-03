6effdfebf064fac4989be8576c4e1fb7a0105525
michael.brooks11 <michael.brooks11@e8b6d1ee-b643-0410-9178-bfabf5f736f5>
2008-02-03 20:03:46 +0000

    Added a pointer-constructor for NxaJointLimitDescription and added NxaJointLimitPairDescription (needed for NxaRevoluteJoint at least, not yet added).

--- trunk/Library/PhysXCPP/NxaJointLimitPairDescription.cpp
+++ trunk/Library/PhysXCPP/NxaJointLimitPairDescription.cpp
@@ -1,19 +1,51 @@
 #include "StdAfx.h"
+
+#include "Nxp.h"
+#include "NxJointLimitPairDesc.h"
+
 #include "NxaJointLimitPairDescription.h"
-
-
 
 NxaJointLimitPairDescription::NxaJointLimitPairDescription(void)
 {
-
+	nxJointLimitPairDesc = new NxJointLimitPairDesc();
 }
 
 NxaJointLimitPairDescription::~NxaJointLimitPairDescription(void)
 {
-
+	this->!NxaJointLimitPairDescription();
 }
 
 NxaJointLimitPairDescription::!NxaJointLimitPairDescription(void)
 {
+	delete nxJointLimitPairDesc;
+}
 
+NxaJointLimitDescription^ NxaJointLimitPairDescription::Low::get()
+{
+	return gcnew NxaJointLimitDescription(&(nxJointLimitPairDesc->low));
+}
+
+void NxaJointLimitPairDescription::Low::set(NxaJointLimitDescription^ value)
+{
+	nxJointLimitPairDesc->low = *(value->nxJointLimitDesc);
+}
+
+NxaJointLimitDescription^ NxaJointLimitPairDescription::High::get()
+{
+	return gcnew NxaJointLimitDescription(&(nxJointLimitPairDesc->high));
+}
+
+void NxaJointLimitPairDescription::High::set(NxaJointLimitDescription^ value)
+{
+	nxJointLimitPairDesc->high = *(value->nxJointLimitDesc);
+}
+
+void NxaJointLimitPairDescription::SetToDefault()
+{
+	nxJointLimitPairDesc->setToDefault();
+}
+
+bool NxaJointLimitPairDescription::IsValid()
+{
+	return nxJointLimitPairDesc->isValid();
 }
--- trunk/Library/PhysXCPP/NxaJointLimitPairDescription.h
+++ trunk/Library/PhysXCPP/NxaJointLimitPairDescription.h
@@ -1,9 +1,31 @@
 #pragma once
 
+#include "NxaJointLimitDescription.h"
+
+class NxJointLimitPairDesc;
+
 public ref class NxaJointLimitPairDescription
 {
+internal:
+	NxJointLimitPairDesc* nxJointLimitPairDesc;
+
 public:
-	NxaJointLimitPairDescription(void);
-	~NxaJointLimitPairDescription(void);
-	!NxaJointLimitPairDescription(void);
+	NxaJointLimitPairDescription();
+	~NxaJointLimitPairDescription();
+	!NxaJointLimitPairDescription();
+
+	property NxaJointLimitDescription^ Low
+	{
+		NxaJointLimitDescription^ get();
+		void set(NxaJointLimitDescription^ value);
+	}
+
+	property NxaJointLimitDescription^ High
+	{
+		NxaJointLimitDescription^ get();
+		void set(NxaJointLimitDescription^ value);
+	}
+
+	void SetToDefault();
+	bool IsValid();
 };
